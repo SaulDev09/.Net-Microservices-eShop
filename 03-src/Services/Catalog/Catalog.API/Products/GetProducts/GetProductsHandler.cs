684afd09c1268a3b490ba310994ec9b25c2009a4
Saul Chipana <Saul.Dev09@gmail.com>
2025-06-18 05:30:09 +0200

    🛠️ 15 | CatalogAPI | Global | Logging Pipeline Behavior

--- 03-src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ 03-src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -5,12 +5,11 @@
     public record GetProductsResult(IEnumerable<Product> Products);
 
 
-    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    internal class GetProductsQueryHandler(IDocumentSession session)
         : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Handling GetProductsQuery {@Query}", query);
             var products = await session.Query<Product>().ToListAsync(cancellationToken);
             return new GetProductsResult(products);
         }
