57e4f2007aafa549dbad2c40668fb2d550f9fd25
Saul Chipana <Saul.Dev09@gmail.com>
2025-07-08 05:14:59 +0200

    🛠️ 37 | DiscountGrpc | GetDiscount

--- 03-src/Services/Discount/Discount.Grpc/Program.cs
+++ 03-src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,5 @@
 using Discount.Grpc.Data;
+using Discount.Grpc.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //app.MapGrpcService<GreeterService>();
 app.UserMigration();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ 03-src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,26 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService
+        (DiscountContext dbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName, context.CancellationToken);
+
+            if (coupon is null)
+                coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount: {}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+    }
+}
