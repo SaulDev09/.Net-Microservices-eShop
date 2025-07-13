9b686a5d56b079b73fc19c20b2c10aed2fe0f02b
Saul Chipana <Saul.Dev09@gmail.com>
2025-07-13 03:49:32 +0200

    🧺 43 | BasketAPI | Consuming Discount gRPC | docker-compose

--- 03-src/Services/Basket/Basket.API/Program.cs
+++ 03-src/Services/Basket/Basket.API/Program.cs
@@ -33,6 +33,14 @@ builder.Services.AddStackExchangeRedisCache(opts =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opt =>
 {
     opt.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
 });
 
 
